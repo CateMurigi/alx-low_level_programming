03b485aad8b4ae13047b3454345d3d787e163147
CateMurigi <murigicatherine03@gmail.com>
2023-02-18 10:02:37 -0800

    update

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char x;
+	
+	for (x = 0; x < 10; x++)
+       {
+	       putchar(x);
+       }
+	putchar('\n');
+	return (0);
+}
