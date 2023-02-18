6c19eed4728769221ee0a8ca57702a615bb511f4
CateMurigi <murigicatherine03@gmail.com>
2023-02-18 07:28:11 -0800

    update

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,13 +9,12 @@ int main(void)
 
 	char c;
 
-	for (c = 'a'; c <= 'z')
+	for (c = 'a'; c <= 'z'; c++)
 	{
 		if (c != 'e' && c != 'q')
 		{
 			putchar(c);
 		}
-		c++;
 	}
 	putchar('\n');
 	return (0);
