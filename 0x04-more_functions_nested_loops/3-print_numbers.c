45c8b0b57d0ef21ffe131428395c3023138dda8a
CateMurigi <murigicatherine03@gmail.com>
2023-02-23 11:07:56 -0800

    task3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the number
+ *
+ * Return: the number
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 4; x <= 0; x++)
+	{
+		_putchar (x + '0');
+	}
+	_putchar ('\n');
+}
